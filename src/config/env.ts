244a4bbe0d945fb296121b15f3228ee439cede12
SaharMahmoudDev <elzerossss@gmail.com>
2026-05-04 19:05:26 +0300

    feat(env): add environment validation using Zod - install zod - create env schema - validate env - add error handling for invalid env variables

---
+++ src/config/env.ts
@@ -0,0 +1,40 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+  NEXT_PUBLIC_API_URL:
+  z.url().refine((val) => {
+  try {
+    const url = new URL(val)
+    if(process.env.NODE_ENV === "production") {
+      return url.protocol === "https:"
+    }
+    if(process.env.NODE_ENV === "development") {
+      return url.protocol === "http:" 
+    }
+  } catch {
+    return false
+  }
+}, {
+  message: "Invalid URL format or protocol",
+}),
+  NEXT_PUBLIC_SOCKET_URL: z.url(),
+});
+
+
+const processEnv = {
+  NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  NEXT_PUBLIC_SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL,
+};
+
+
+const result = envSchema.safeParse(processEnv);
+
+if (!result.success) {
+  const errors = result.error.issues
+    .map((issue) => `${issue.path.join(".")}: ${issue.message}`)
+    .join("\n");
+  console.error(`Invalid environment variables:\n${errors}`);
+  throw new Error(`Invalid environment variables:\n${errors}`);
+}
+
+export const env = result.data;
