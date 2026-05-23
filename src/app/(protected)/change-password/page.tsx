77ef5fa40b785b392658dce7a04647fb5201fa45
SaharMahmoudDev <elzerossss@gmail.com>
2026-05-24 00:39:10 +0300

    feat(auth): implement routing guards and proxy protection system
    - add proxy-based route protection
    - add role-based access control
    - add ProtectedRoute component
    - add RoleGuard component
    - add centralized protected routes constants
    - protect guest/authenticated/role routes
    - add verification session handling
    - add callbackUrl redirect support

---
+++ src/app/(protected)/change-password/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChangePasswordPage = () => {
+  return (
+    <></>
+  )
+}
+
+export default ChangePasswordPage
--- src/modules/auth/utils/getDashboardPath.ts
+++ src/modules/auth/utils/getDashboardPath.ts
@@ -1,4 +1,5 @@
-export function getDashboardPath(role?: string) {
+import {UserRole} from "@/modules/auth/types/auth.types"
+export function getDashboardPath(role?: UserRole) {
   if (role === "admin") return "/admin/dashboard";
   if (role === "vendor") return "/vendor/dashboard";
 
