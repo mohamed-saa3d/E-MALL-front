cdb42991488daa244eb09a85eff62eeb15c535db
mohamed-saa3d <mohammedsaad.v7@gmail.com>
2026-07-28 01:33:03 +0300

    fix build errors

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -2,6 +2,8 @@ import AdminLayout from "@/components/layouts/AdminLayout";
 import { RoleGuard } from "@/modules/auth/components/RoleGuard";
 import { WithChildren } from "@/types/common.types";
 
+export const dynamic = 'force-dynamic';
+
 type Props = WithChildren;
 
 const Layout = ({ children }: Props) => {
--- src/app/(vendor)/layout.tsx
+++ src/app/(vendor)/layout.tsx
@@ -2,6 +2,8 @@ import VendorLayout from "@/components/layouts/VendorLayout";
 import { RoleGuard } from "@/modules/auth/components/RoleGuard";
 import { WithChildren } from "@/types/common.types";
 
+export const dynamic = 'force-dynamic';
+
 type Props = WithChildren;
 
 const Layout = ({ children }: Props) => {
